0447f337424096298f2849d648a8bb14fd0788f5
Melvin Versluijs <melvin.versluijs@outlook.com>
2019-10-24 11:14:50 +0200

    Remove bootstrap, lodash, popper and jQuery; Style header.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,70 +11,57 @@
     <title>{{ config('app.name', 'Dashboard') }}</title>
 
     <!-- Styles -->
+    @yield('styles')
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    @yield('header-scripts')
 </head>
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <header class="header">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Dashboard') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
+                <nav class="navbar">
+                    <ul class="navbar-list">
                         @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                            <a class="nav-link" href="{{ route('login') }}">
+                                {{ __('Login') }}
                             </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                    style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">
+                                {{ __('Register') }}
+                            </a>
+                        </li>
+                        @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                {{ Auth::user()->name }}
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <form action="{{ route('logout') }}" method="POST" class="form form-logout">
+                                @csrf
+                                <button type="submit" class="btn btn-logout">{{ __('Logout') }}</button>
+                            </form>
                         </li>
                         @endguest
                     </ul>
-                </div>
+                </nav>
             </div>
-        </nav>
+        </header>
 
         <main class="content">
             @yield('content')
         </main>
     </div>
     <!-- Scripts -->
+    @yield('footer-scripts')
     <script src="{{ mix('/js/app.js') }}" defer></script>
 </body>
 

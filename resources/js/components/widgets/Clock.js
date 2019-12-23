102b740ebd547eedc77a212dbd3ef85c92e62119
Melvin Versluijs <melvin.versluijs@outlook.com>
2019-12-23 20:22:46 +0100

    Update dependencies; Add leading zero's to clock widget.

--- resources/js/components/widgets/Clock.js
+++ resources/js/components/widgets/Clock.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 /**
  * Clock widget component.
@@ -13,11 +13,23 @@ const Clock = () => {
 
     return (
         <div className="clock digital">
-            <span className="hours">{date.getHours()}</span>
+            <span className="hours">
+                {date.getHours() >= 10
+                    ? date.getHours()
+                    : `0${date.getHours()}`}
+            </span>
             <span className="separator">:</span>
-            <span className="minutes">{date.getMinutes()}</span>
+            <span className="minutes">
+                {date.getMinutes() >= 10
+                    ? date.getMinutes()
+                    : `0${date.getMinutes()}`}
+            </span>
             <span className="separator">:</span>
-            <span className="seconds">{date.getSeconds()}</span>
+            <span className="seconds">
+                {date.getSeconds() >= 10
+                    ? date.getSeconds()
+                    : `0${date.getSeconds()}`}
+            </span>
         </div>
     );
 };
@@ -26,7 +38,7 @@ const Clock = () => {
 export default Clock;
 
 // Insert component in element with id: 'clock'
-const element = document.getElementById('clock');
+const element = document.getElementById("clock");
 if (element) {
     ReactDOM.render(<Clock />, element);
 }

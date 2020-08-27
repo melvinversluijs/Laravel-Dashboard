59ae8e625f22cab4df35c6f65c1098e08cdb0113
Melvin Versluijs <melvin.versluijs@outlook.com>
2020-08-27 19:53:28 +0200

    Update main blade file

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,39 +1,32 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <!-- Title -->
     <title>{{ config('app.name', 'Laravel') }} @yield('title-suffix')</title>
-
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
-
 <body class="bg-gray-100">
-    <div id="app">
-        <nav class="border-b border-gray-400 py-2">
-            <div class="container flex justify-between items-center">
-                <div class="nav__right ">
-                    <a href="{{ url('/') }}" class="font-bold text-xl hover:text-green-400">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="nav__left">
-                    <ul class="flex">
-                        <!-- Authentication Links -->
-                        @guest
+<div id="app">
+    <nav class="border-b border-gray-400 py-2">
+        <div class="container flex justify-between items-center">
+            <div class="ml-8">
+                <a href="{{ url('/') }}" class="font-bold text-xl hover:text-green-400">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+            </div>
+            <div class="mr-8">
+                <ul class="flex">
+                    <!-- Authentication Links -->
+                    @guest
                         <li class="pr-2">
                             <a href="{{ route('login') }}" class="hover:text-green-400">
                                 {{ __('Login') }}
@@ -44,28 +37,26 @@
                                 {{ __('Register') }}
                             </a>
                         </li>
-                        @else
+                    @else
                         <li>
                             <a>{{ Auth::user()->name }}</a>
-
-                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
+                            <a
+                                href="{{ route('logout') }}" onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
                                 @csrf
                             </form>
                         </li>
-                        @endguest
-                    </ul>
-                </div>
+                    @endguest
+                </ul>
             </div>
-        </nav>
-
-        <main>
-            @yield('content')
-        </main>
-    </div>
+        </div>
+    </nav>
+    <main>
+        @yield('content')
+    </main>
+</div>
 </body>
-
 </html>
